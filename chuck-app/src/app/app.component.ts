42f14a11aee0ae70e6af91907aaa62ac9590b45a
Gabriel Rusu <Gabriel.Rusu@centric.eu>
2022-09-28 12:50:56 +0300

    lifecycle hooks, ngif,for

--- chuck-app/src/app/app.component.ts
+++ chuck-app/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'chuck-app';
+export class AppComponent implements OnInit{
+  
+  cardTitle: string = '';
+  displayCard: boolean = false;
+  jokeArray: string[] = ['joke1', 'joke2', 'joke3'];
+
+  ngOnInit(): void {
+    setTimeout(()=>{
+      this.cardTitle = 'New title';
+    }, 3000);
+  }
+
+  buttonClicked(event:any){
+    console.log(event);
+  }
+
+  showCard(){
+    this.displayCard = true;
+  }
 }
--- chuck-app/src/app/joke-details/joke-card/joke-card.component.ts
+++ chuck-app/src/app/joke-details/joke-card/joke-card.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-joke-card',
   templateUrl: './joke-card.component.html',
   styleUrls: ['./joke-card.component.scss']
 })
-export class JokeCardComponent implements OnInit {
+export class JokeCardComponent implements OnInit, OnChanges {
 
-  title:string = 'Some title';
+  @Input() title: string = 'Some title';
+  @Output() likeClicked: EventEmitter<string> = new EventEmitter();
 
   today: Date = new Date();
 
@@ -19,6 +20,14 @@ export class JokeCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log('component initialized');
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+  }
+
+  onLikeClick() {
+    this.likeClicked.emit('like clicked');
+  }
 }
